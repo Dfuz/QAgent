dde1c262e3738b96040c7aa967653e3e9078d066
Vadim Perfilyev <vadimperfilev@gmail.com>
2021-03-31 00:24:51 +0300

    added handshake

--- src/qagent.cpp
+++ src/qagent.cpp
@@ -1,6 +1,40 @@
 #include "qagent.h"
 
+
 QAgent::QAgent(QObject *parent) : QObject(parent)
 {
+    initSocket();
+    timer.callOnTimeout(this, &QAgent::startSend);
+}
 
+int QAgent::getCompression()
+{
+    return compression;
+}
+
+void QAgent::setCompression(int newCompress)
+{
+    compression = newCompress;
+}
+
+void QAgent::initSocket()
+{
+    auto tcpSocket = std::make_shared<QTcpSocket>();
+    tcpSocket->connectToHost(serverAddress, serverPort);
+    query = std::make_unique<Utils::QueryBuilder>(tcpSocket);
+}
+
+void QAgent::startSend()
+{
+
+}
+
+void QAgent::performHandshake(std::shared_ptr<Utils::QueryBuilder> _query)
+{
+    auto message = Utils::ServiceMessage{R"({"who":"agent"})"};
+    auto msg = _query->makeQueryRead()
+           .toGet<Utils::Service>()
+           .toSend(message)
+           .invoke();
+    return;
 }
--- src/qagent.h
+++ src/qagent.h
@@ -1,13 +1,31 @@
 #ifndef QAGENT_H
 #define QAGENT_H
 
+#include "common/querybuilder.h"
+
 #include <QObject>
+#include <QTcpSocket>
+#include <QHostAddress>
+#include <QTimer>
+#include <memory>
 
 class QAgent : public QObject
 {
     Q_OBJECT
+private:
+    QTimer timer;
+    quint16 serverPort{0};
+    QHostAddress serverAddress{QHostAddress::Null};
+    std::unique_ptr<Utils::QueryBuilder> query;
+    inline static int compression;
+    void initSocket();
+    void startSend();
+    void performHandshake(std::shared_ptr<Utils::QueryBuilder> _query);
 public:
     explicit QAgent(QObject *parent = nullptr);
+    void init();
+    static int getCompression(void);
+    static void setCompression(int newCompress);
     friend class qagent_tests;
 
 signals:
