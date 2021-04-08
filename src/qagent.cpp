dde72c157f63c73c39680c597a65cd4e2e13d4ee
Vadim Perfilyev <vadimperfilev@gmail.com>
2021-04-09 00:38:15 +0300

    added read config and some methods prototypes

--- src/qagent.cpp
+++ src/qagent.cpp
@@ -1,10 +1,48 @@
 #include "qagent.h"
-
+#include "common/utils.h"
 
 QAgent::QAgent(QObject *parent) : QObject(parent)
 {
     initSocket();
-    timer.callOnTimeout(this, &QAgent::startSend);
+    timer.callOnTimeout(this, &QAgent::startCollectData);
+
+}
+
+void QAgent::readConfig(QString settings_path)
+{
+    QSettings settings(settings_path, Utils::JsonFormat);
+
+    settings_path = settings.fileName();
+
+    if (!settings.value("ListenPort").isNull())
+        listenPort = settings.value("ListenPort").toUInt();
+
+    if (!settings.value("ListenIP").isNull())
+        listenIP = QHostAddress{settings.value("ListenIP").toString()};
+
+    if (!settings.value("BufferSize").isNull())
+        bufferSize = settings.value("BufferSize").toUInt();
+
+    if (!settings.value("RefreshActiveChecks").isNull())
+        refreshActiveChecks = duration_cast<seconds>
+                (
+                    Utils::parseTime(settings.value("RefreshActiveChecks")
+                                     .toString()
+                                     )
+                );
+}
+
+bool QAgent::startListen()
+{
+    connect(&localServer, &QTcpServer::newConnection, this, &QAgent::performPassiveCheck);
+
+    if (!localServer.listen(listenIP, listenPort))
+    {
+        qDebug() << "Unable to start listen";
+        return false;
+    }
+    qDebug() << "Listening started";
+    return true;
 }
 
 int QAgent::getCompression()
@@ -17,15 +55,24 @@ void QAgent::setCompression(int newCompress)
     compression = newCompress;
 }
 
+bool QAgent::performPassiveCheck()
+{
+    // TODO
+
+
+    return false;
+}
+
 void QAgent::initSocket()
 {
     auto tcpSocket = std::make_shared<QTcpSocket>();
-    tcpSocket->connectToHost(serverAddress, serverPort);
+    tcpSocket->connectToHost(serverIP, serverPort);
     query = std::make_unique<Utils::QueryBuilder>(tcpSocket);
 }
 
-void QAgent::startSend()
+void QAgent::startCollectData()
 {
+
 }
 
 void QAgent::performHandshake(std::shared_ptr<Utils::QueryBuilder> _query)
--- src/qagent.h
+++ src/qagent.h
@@ -6,28 +6,46 @@
 #include <QObject>
 #include <QTcpSocket>
 #include <QHostAddress>
+#include <QDebug>
+#include <QTcpServer>
 #include <QTimer>
 #include <memory>
+#include <chrono>
+
+using namespace std::chrono;
 
 class QAgent : public QObject
 {
     Q_OBJECT
 private:
+    // Поля
     QTimer timer;
     quint16 serverPort{0};
-    QHostAddress serverAddress{QHostAddress::Null};
+    quint16 listenPort{0}; // агент будет слушать этот порт для подключений с сервера; диапазон 1024-32767
+    QHostAddress serverIP{QHostAddress::Null};
+    QHostAddress listenIP{QHostAddress::LocalHost};
+    QString hostName; // уникальное, регистрозависимое имя хоста
     std::unique_ptr<Utils::QueryBuilder> query;
+    QTcpServer localServer;
     inline static int compression;
+    quint16 bufferSize = 100; // максимальное количество значений в буфере памяти
+    std::chrono::seconds refreshActiveChecks{60s};
+
+    // Методы
     void initSocket();
-    void startSend();
+    void startCollectData();
     void performHandshake(std::shared_ptr<Utils::QueryBuilder> _query);
 public:
     explicit QAgent(QObject *parent = nullptr);
+    void readConfig(QString settings_path = "conf.json");
+    bool startListen();
     void init();
     static int getCompression(void);
     static void setCompression(int newCompress);
     friend class qagent_tests;
 
+private slots:
+    bool performPassiveCheck();
 signals:
 
 };
